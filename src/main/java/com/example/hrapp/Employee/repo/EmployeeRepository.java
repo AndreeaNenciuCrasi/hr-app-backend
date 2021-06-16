e400a92ca346e5948075b07349878acb67856fcb
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-06-16 14:51:56 +0300

    adds employee service class

--- src/main/java/com/example/hrapp/Employee/repo/EmployeeRepository.java
+++ src/main/java/com/example/hrapp/Employee/repo/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.example.hrapp.Employee.repo;
 
 import com.example.hrapp.Employee.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 }
---
+++ src/main/java/com/example/hrapp/Employee/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.example.hrapp.Employee.service;
+
+import com.example.hrapp.Employee.model.Employee;
+import com.example.hrapp.Employee.repo.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepository.save(employee);
+    }
+}
