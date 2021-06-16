63f2ab6a2da5eabcf8e28ad62e540f437821f671
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2021-06-16 15:17:48 +0300

    adds controller

---
+++ src/main/java/com/example/hrapp/Employee/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.example.hrapp.Employee.controller;
+
+import com.example.hrapp.Employee.model.Employee;
+import com.example.hrapp.Employee.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees(){
+        List<Employee> employees = employeeService.findAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+}
